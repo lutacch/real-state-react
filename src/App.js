c0cc461051fc3b3c2dff42c29c6db983d47f650c
Lucia Tacchetti <lucia@MacBook-Air-de-Lucia.local>
2022-12-27 19:48:38 +0100

    real state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Best from "./components/best/Best";
+import Featured from "./components/featured/Featured";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Best/>
+      <Featured/>
     </div>
   );
 }
